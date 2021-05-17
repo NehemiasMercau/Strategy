0fe93b30a0d3a8025c5f69fa593e9fdf763c5b76
Nehemias <mercau.nehemias@gmail.com>
2021-05-17 00:28:50 -0300

    todo listo

--- strategy/src/main/java/ing_soft/strategy/Character.java
+++ strategy/src/main/java/ing_soft/strategy/Character.java
@@ -1,11 +1,12 @@
 package ing_soft.strategy;
+
 public abstract class Character {
 	WeaponBehavior weapon;
 
 	public void setWeapon(WeaponBehavior w) {
 		this.weapon = w;
 	}
-	
+
 	public void fight() {
 		System.out.println(weapon.useWeapon());
 	}
